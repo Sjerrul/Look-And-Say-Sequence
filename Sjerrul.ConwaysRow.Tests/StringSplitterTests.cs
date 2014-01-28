167e731a04a28d4a3b1a0182062dccaa76863524
Sjerrul <cerylwiltink@hotmail.com>
2014-01-28 17:26:36 +0100

    First commit, basic functionality working

---
+++ Sjerrul.ConwaysRow.Tests/StringSplitterTests.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Sjerrul.ConwaysRow.Tests
+{
+    [TestClass]
+    public class StringSplitterTests
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            string input = "1";
+
+            IList<string> result = StringSplitter.Split(input);
+            IList<string> expected = new List<string>
+            {
+                "1"
+            };
+
+            Assert.IsTrue(AreArraysEqual(expected, result));
+        }
+
+        [TestMethod]
+        public void Test11()
+        {
+            string input = "11";
+
+            IList<string> result = StringSplitter.Split(input);
+            IList<string> expected = new List<string>
+            {
+                "11"
+            };
+
+            Assert.IsTrue(AreArraysEqual(expected, result));
+        }
+
+        [TestMethod]
+        public void Test111()
+        {
+            string input = "111";
+
+            IList<string> result = StringSplitter.Split(input);
+            IList<string> expected = new List<string>
+            {
+                "111"
+            };
+
+            Assert.IsTrue(AreArraysEqual(expected, result));
+        }
+
+        [TestMethod]
+        public void Test12()
+        {
+            string input = "12";
+
+            IList<string> result = StringSplitter.Split(input);
+            IList<string> expected = new List<string>
+            {
+                "1",
+                "2"
+            };
+
+            Assert.IsTrue(AreArraysEqual(expected, result));
+        }
+
+        private bool AreArraysEqual(IList<string> array1, IList<string> array2)
+        {
+            if (array1.Count != array2.Count)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < array1.Count; i++)
+            {
+                if (array1[i] != array2[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        [TestMethod]
+        public void Test112()
+        {
+            string input = "112";
+
+            IList<string> result = StringSplitter.Split(input);
+            IList<string> expected = new List<string>
+            {
+                "11",
+                "2"
+            };
+
+            Assert.IsTrue(AreArraysEqual(expected, result));
+        }
+
+        [TestMethod]
+        public void Test1122()
+        {
+            string input = "1122";
+
+            IList<string> result = StringSplitter.Split(input);
+            IList<string> expected = new List<string>
+            {
+                "11",
+                "22"
+            };
+
+            Assert.IsTrue(AreArraysEqual(expected, result));
+        }
+
+        [TestMethod]
+        public void Test112233()
+        {
+            string input = "112233";
+
+            IList<string> result = StringSplitter.Split(input);
+            IList<string> expected = new List<string>
+            {
+                "11",
+                "22",
+                "33"
+            };
+
+            Assert.IsTrue(AreArraysEqual(expected, result));
+        }
+
+        [TestMethod]
+        public void Test11223311()
+        {
+            string input = "11223311";
+
+            IList<string> result = StringSplitter.Split(input);
+            IList<string> expected = new List<string>
+            {
+                "11",
+                "22",
+                "33",
+                "11"
+            };
+
+            Assert.IsTrue(AreArraysEqual(expected, result));
+        }
+        
+    }
+}
---
+++ Sjerrul.ConwaysRow/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sjerrul.ConwaysRow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Conway's Row : Look-and-Say Sequence");
+            Console.WriteLine("================================");
+            Console.WriteLine("Try to guess the next number in the series!");
+            Console.WriteLine("Press any key to generate the next number in the sequence, enter 'q' to quit.");
+            Console.Write("Input starting number: ");
+            string currentNumber = Console.ReadLine();
+
+            Console.WriteLine(String.Format("Starting with number {0}. Press Enter to ", currentNumber));            
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "q")
+                {
+                    break;
+                }
+
+                string nextNumber = GenerateNextNumber(currentNumber);
+                Console.WriteLine(nextNumber);
+                currentNumber = nextNumber;
+            }
+            Console.WriteLine("Application closed.");
+        }
+
+        private static string GenerateNextNumber(string currentNumber)
+        {
+            IList<String> elements = StringSplitter.Split(currentNumber);
+
+            StringBuilder nextNumber = new StringBuilder();
+            foreach (string s in elements)
+            {
+                char number = s[0];
+                int amount = s.Length;
+
+                nextNumber.Append(amount).Append(number);
+            }
+
+            return nextNumber.ToString();
+        }
+
+
+    }
+}
